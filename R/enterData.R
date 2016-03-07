ca95db7478730cc9a5104c8f936cf4c4d44f1195
jakobschumacher <jakob.schumacher@web.de>
2016-03-07 20:49:56 +0100

    Case studies dataset campy

--- R/enterData.R
+++ R/enterData.R
@@ -8,27 +8,25 @@ enterData <- function(filename="quest.csv", path="data/") {
     if(!dir.exists(path)) dir.create(path)
     # Check if file exists if not creates one
     if(!file.exists(file)) file.create(file=file)
-     # Check if variable database exists and file size is bigger than 0
-     if (!exists("database") & file.info(file)$size!=0) database <- read.csv(file=file)
-     # Check if variable database exists otherwise creates one
-     if (!exists("database")) database <- data.frame()
+    # Check if variable database exists and file size is bigger than 0
+    if (!exists("database") & file.info(file)$size!=0) database <- read.csv(file=file)
+    # Check if variable database exists otherwise creates one
+    if (!exists("database")) database <- data.frame()
 
     for(i in 1:500) {
-    #---------------------- Start the questionnaire ----------------------------
-    id <- as.character(readline("id: "))
-    pseudonym <- as.character(readline("pseudonym: "))
-    age <- as.numeric(readline("age: "))
-    sex <- readline("sex: ")
+        #---------------------- Start the questionnaire ----------------------------
+        id <- as.character(readline("id: "))
+        pseudonym <- as.character(readline("pseudonym: "))
+        age <- as.numeric(readline("age: "))
+        sex <- readline("sex: ")
 
-    #-------------- Appends database from one questionnaire -------------------
-    database$sex <- NULL
-    database <- rbind(database,data.frame(id, pseudonym, age, sex))
-    rm(id, pseudonym, age, sex)
-    #-------------- Next questionnaire -------------------
-    if(readline("Do you want to go on? (leave empty to proceed):")!="") break
+        #-------------- Appends database from one questionnaire -------------------
+        database <- rbind(database,data.frame(id, pseudonym, age, sex))
+        rm(id, pseudonym, age, sex)
+        #-------------- Next questionnaire -------------------
+        if(readline("Do you want to go on? (leave empty to proceed):")!="") break
     }
 
     write.csv(database, file=file, row.names = FALSE)
 
 }
-enterData()
